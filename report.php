df5a6705fdfc6b86bce304f332c247dfb2d65eb2
Dan Niemitalo <dniemitalo@linnmar.k12.ia.us>
2017-01-08 16:43:20 -0600

    Report improvements
    Loop through fields to generate report table.

--- report.php
+++ report.php
@@ -2,15 +2,28 @@
 ini_set('display_errors', 'On');
 error_reporting(E_ALL | E_STRICT);
 require_once 'db.php';
+
 $fields = 'ID, matchnum, team';
-//$field_list = preg_split ('/[\s*,\s*]*,+[\s*,\s*]*/', $fields);
+$field_list = preg_split ('/[\s*,\s*]*,+[\s*,\s*]*/', $fields);
+
+//Create header row for report
+echo "<table>\n<tr>";
+foreach($field_list as $fieldname){
+	echo "<th>".$fieldname."</th>";	
+}
+echo "</tr>\n";
+
+//Create a table of match results, looping through every field of every row
 $sql = "SELECT $fields FROM matches";
 if($result = mysqli_query($conn,$sql)){
 	while($row = mysqli_fetch_assoc($result)){
-		//echo $row['ID'].", ".$row['matchnum'].", ".$row['team']."<br>";		
-		echo implode(", ", $row)."<br";
+		echo "<tr>";
+		foreach($field_list as $fieldname){
+			echo "<td>".$row[$fieldname]."</td>";
+		}
+		echo "</tr>\n";
 	}
 }
-
+echo "</table>";
 mysqli_close($conn);
 ?>
