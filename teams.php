fdf934612f72c2498176afa7b82f331c05ebf79f
Dan Niemitalo <dniemitalo@linnmar.k12.ia.us>
2017-01-15 20:37:37 -0600

    Added team list
    Possible future use as a linked list for navigating to single team
    reports.

---
+++ teams.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html>
+<body>
+<table><tr><th>Team</th><th>Nickname</th><th>Location</th></tr>
+<?php
+//Start on character 15, because the JS file includes "var teamData = " that we wish to ignore here.
+$string =substr(file_get_contents("teamData.js"),15);
+$teams = json_decode($string, true);
+//sort array here
+
+foreach($teams as $t){
+	$teamnum = $t['team_number'];
+	$nickname = $t['nickname'];
+	$locality = $t['locality'];
+	$region = $t['region'];
+	echo "<tr><td>$teamnum</td><td>$nickname</td><td>{$locality}, {$region}</td></tr>\n";
+}
+
+// $blue1 = $sched[36]['alliances']['blue']['teams'][0];
+// echo "Blue 1: ".$blue1;
+//echo "<br>".gettype($sched);
+?>
+</table>
+</body>
+</html>
