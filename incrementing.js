820a7ddd6652dada2eb74f47d39b6623644ed68b
RSchneyer <reschneyer89@gmail.com>
2017-02-04 09:43:52 -0600

    jQuery Button Code
    Added jQuery code for +/- 1&5 buttons

--- incrementing.js
+++ incrementing.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+//Auto High Made
 	$("#minusFiveautohighmade").click(function(){
 		changeBy("auto_high_made",-5);
 	});
@@ -11,7 +12,7 @@ $(document).ready(function(){
 	$("#plusFiveautohighmade").click(function(){
 		changeBy("auto_high_made",5);
 	});
-
+//Auto High Miss
 	$("#minusFiveautohighmiss").click(function(){
 		changeBy("auto_high_miss",-5);
 	});
@@ -23,6 +24,84 @@ $(document).ready(function(){
 	});
 	$("#plusFiveautohighmiss").click(function(){
 		changeBy("auto_high_miss",5);
+	});
+//Auto Low Made
+	$("#minusFiveautolowmade").click(function(){
+		changeBy("auto_low_made",-5);
+	});
+	$("#minusOneautolowmade").click(function(){
+		changeBy("auto_low_made",-1);
+	});
+	$("#plusOneautolowmade").click(function(){
+		changeBy("auto_low_made",1);
+	});
+	$("#plusFiveautolowmade").click(function(){
+		changeBy("auto_low_made",5);
+	});
+//Auto Low Miss
+	$("#minusFiveautolowmiss").click(function(){
+		changeBy("auto_low_miss",-5);
+	});
+	$("#minusOneautolowmiss").click(function(){
+		changeBy("auto_low_miss",-1);
+	});
+	$("#plusOneautolowmiss").click(function(){
+		changeBy("auto_low_miss",1);
+	});
+	$("#plusFiveautolowmiss").click(function(){
+		changeBy("auto_low_miss",5);
+	});
+//Tele High Made
+	$("#minusFivetelehighmade").click(function(){
+		changeBy("tele_high_made",-5);
+	});
+	$("#minusOnetelehighmade").click(function(){
+		changeBy("tele_high_made",-1);
+	});
+	$("#plusOnetelehighmade").click(function(){
+		changeBy("tele_high_made",1);
+	});
+	$("#plusFivetelehighmade").click(function(){
+		changeBy("tele_high_made",5);
+	});
+//Tele High Miss
+	$("#minusFivetelehighmiss").click(function(){
+		changeBy("tele_high_miss",-5);
+	});
+	$("#minusOnetelehighmiss").click(function(){
+		changeBy("tele_high_miss",-1);
+	});
+	$("#plusOnetelehighmiss").click(function(){
+		changeBy("tele_high_miss",1);
+	});
+	$("#plusFivetelehighmiss").click(function(){
+		changeBy("tele_high_miss",5);
+	});
+//Tele Low Made
+	$("#minusFivetelelowmade").click(function(){
+		changeBy("tele_low_made",-5);
+	});
+	$("#minusOnetelelowmade").click(function(){
+		changeBy("tele_low_made",-1);
+	});
+	$("#plusOnetelelowmade").click(function(){
+		changeBy("tele_low_made",1);
+	});
+	$("#plusFivetelelowmade").click(function(){
+		changeBy("tele_low_made",5);
+	});
+//Tele Low Miss
+	$("#minusFivetelelowmiss").click(function(){
+		changeBy("tele_low_miss",-5);
+	});
+	$("#minusOnetelelowmiss").click(function(){
+		changeBy("tele_low_miss",-1);
+	});
+	$("#plusOnetelelowmiss").click(function(){
+		changeBy("tele_low_miss",1);
+	});
+	$("#plusFivetelelowmiss").click(function(){
+		changeBy("tele_low_miss",5);
 	});
 });
 
